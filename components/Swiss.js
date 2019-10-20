4ffbf0cf1b11877d4bfb9f9fbf98895b33d06f2d
markclynch <mark.christinat@gmail.com>
2019-10-19 17:39:34 -0700

    conditional rendering pow.png

--- components/Swiss.js
+++ components/Swiss.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { StyleSheet, View, Image, TouchableWithoutFeedback } from 'react-native'
 
 const Swiss = props => {
-  const { index, handlePress } = props
-  return (
+  const { index, handlePress, wackable } = props
+  return wackable ? (
     <View>
       <TouchableWithoutFeedback onPress={() => handlePress(index)}>
         <Image source={require('../images/Swiss.png')} style={styles.holes} />
       </TouchableWithoutFeedback>
+    </View>
+  ) : (
+    <View>
+      <Image source={require('../images/pow.png')} style={styles.holes} />
     </View>
   )
 }
